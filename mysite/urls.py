89fb551becbdddc1ae8e3a6354870bab59524570
gooldeer <moysa.sergey@gmail.com>
2013-11-12 23:02:25 +0200

    fixing bugs with starting.

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 
-from youtubetomp3 import views
+# from youtubetomp3 import views
 
 admin.autodiscover()
 
@@ -23,6 +23,6 @@ urlpatterns = patterns('',
     
     url(r'^', include('youtubetomp3.urls', namespace="youtubetomp3")),
 
-    url(r'^accounts/profile/$', views.profile, name="profile_detail"),
+    # url(r'^accounts/profile/$', views.profile, name="profile_detail"),
     url(r'^accounts/', include('registration.backends.default.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
