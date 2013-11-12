d350b8864d303278eba555aeb3104db285c4c942
gooldeer <moysa.sergey@gmail.com>
2013-11-13 00:15:18 +0200

    created postrgre sql db

--- mysite/settings.py
+++ mysite/settings.py
@@ -4,18 +4,18 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Sergey Moysa', 'moysa.sergey@you2mp3.com'),
+    ('Sergey Moysa', 'sergey@gagandeep.webfactional.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'home/gagandeep/webapps/gettubemusic/gettubemusic/sqlite3.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'gooldeer',
-        'PASSWORD': 'EH3j6Gc9',
+        'USER': 'db_admin',
+        'PASSWORD': 'youtubedownload',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
