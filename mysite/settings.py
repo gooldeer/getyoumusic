98cd868c9223e854876bbd8c1766ca14bfc7fa0c
gooldeer <gagandeep@web432.webfaction.com>
2013-11-13 15:44:32 +0000

    Changed media root

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,7 +58,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/gagandeep/webapps/gettubemusic/gettubemusic/media/'
+MEDIA_ROOT = '/home/gagandeep/webapps/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
